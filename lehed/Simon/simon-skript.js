c53b511f0f405695ea7a7e3673eff4bc0884f9f7
bimonXdd <saimon.prii@gmail.com>
2022-11-04 18:02:56 +0200

    chuck api enam vahem

---
+++ lehed/Simon/simon-skript.js
@@ -0,0 +1,22 @@
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-RapidAPI-Key': 'b329f033ccmshc0e31e9d4a46e5ap1cf3b2jsne0c37ab14dff',
+		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+	}
+};
+
+function nali(){
+	fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
+		.then(response => response.json()) 
+		.then(response => document.getElementById("info").innerHTML = response.value)
+		
+		.catch(err => console.error(err));
+
+};
+
+
+
+
+
