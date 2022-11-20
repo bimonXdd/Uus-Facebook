dc20aa6c85b1945e3052184c3066b2291e209f69
bimonXdd <saimon.prii@gmail.com>
2022-11-20 15:46:39 +0200

    mdea tehtud

--- lehed/Simon/API2/API2.js
+++ lehed/Simon/API2/API2.js
@@ -8,7 +8,7 @@ function excuse(){
 
 fetch('https://excuser.herokuapp.com/v1/excuse', option)
     .then(response => response.json())
-    .then(response => document.getElementById("excuse").innerHTML = (response[0].excuse))
+    .then(response => document.getElementById("text").innerHTML = (response[0].excuse))
     .catch(error => console.log('Midagi on pekkis'))
 
 }
