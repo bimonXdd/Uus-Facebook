10b5067cf14db332a8871218e1e07efe9f217fbe
Madis <77383233+madis025@users.noreply.github.com>
2022-11-27 15:46:14 +0200

    uuendus

--- madis-skript.js
+++ madis-skript.js
@@ -1,9 +1,3 @@
 function otsi() {
-    document = "https://www.pistik.net/huumor/anekdoot/1900";
-    let jupp = document.createDocumentFragment();
-    let temp = document.createElement("div");
-    temp.innerHTML = html;
-    jupp.appendChild(temp);
-    var tekst = jupp.querySelector("section-box");
-    document.querySelector('.anekdoot').append(tekst)
+    alert("Töötab");
 }
