0b00f3bf3174f0cc8aec14929412437e80e5bba7
Madis <77383233+madis025@users.noreply.github.com>
2022-11-27 14:35:31 +0200

    mida iganes

--- madis-skript.js
+++ madis-skript.js
@@ -1,1 +1,9 @@
-
+function otsi() {
+    document = "https://www.pistik.net/huumor/anekdoot/1900";
+    let jupp = document.createDocumentFragment();
+    let temp = document.createElement("div");
+    temp.innerHTML = html;
+    jupp.appendChild(temp);
+    var tekst = jupp.querySelector("section-box");
+    document.querySelector('.anekdoot').append(tekst)
+}
