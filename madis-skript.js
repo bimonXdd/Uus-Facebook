3476e927fc4ecfa3ba72e4bad640c469614e89f1
Madis <77383233+madis025@users.noreply.github.com>
2022-12-03 12:18:14 +0200

    tere

--- madis-skript.js
+++ madis-skript.js
@@ -1,3 +1,5 @@
-function otsi() {
-    alert("Töötab");
+ function otsi(){
+    let leht = "https://api.giphy.com/v1/gifs/random?api_key=3225PhfbH5MY04mgEIvX1BMpXq1ysD97&tag=fail&rating=pg-13";
+	
+    
 }
